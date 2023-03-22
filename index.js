a3929b1f725744d4d7c437bd8014567ae00cd8ca
codedcrypt <127953576+codedcrypt@users.noreply.github.com>
2023-03-22 15:01:10 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,152 @@
+// let variablea;
+// let firstName = '543.7'
+// let lastName = 'thomas'
+// let number1 = '42'
+// console.log(typeof variablea);
+// console.log(variablea)
+// const number =3.142
+// let
+
+
+// let age = 35
+// const gravity = 9.81
+// let a = 'you  are welcome';
+// let mass = 72
+// let b = 'error erro error'
+// const pi = Math.PI
+// let c = age + mass * gravity / pi
+// if (console.log(c 'floats'uhuhuu) ) {
+//     console.log(b);
+    
+// } 
+
+// let num1 = 2;
+// let num2 = 5;
+// let name = "john";
+// let str;
+
+// alert(num1 + num2);
+// alert(num1 - num2);
+// alert(num1 * num2);
+// alert(num1 / num2);
+// alert(1/0);
+// alert(infinity);
+
+// alert(typeof num1)
+// alert(typeof num2)
+// alert(typeof num2)
+
+
+// let name = "kevin";
+// alert(`hello ${2}`);
+// alert(`hello ${"name"}`);
+// alert(`hello ${name}`);
+
+// var number = "yello World"
+// console.log(number.length)
+
+// var number = "yello world";
+// var figure = number.indexOf("o")
+// var update = number.replaceAll("o","i")
+// var kk = number.charAt(4)
+
+// console.log(kk);
+
+// var first = "hello";
+// var second = "world";
+// console.log(first + second);
+
+// var number = "10";
+// // console.log(typeof number);
+// var convert = number(number);
+// console.log(typeof );
+
+// var convert = parseInt(number, 10)
+
+
+// var number = "5"
+// var num2 = 50
+// var converted = number(number)
+// console.log(number - num2)
+
+// let dog1E1 = document.querySelector('.dog1')
+
+// let stringNum = "56"
+// dog1E1 = parseInt(50.67);
+// dog1E1 = Number(stringNum)
+// console.log(typeof dog1E1)
+
+// dog1E1.innerHTML=56
+// let dog2 = parseInt(67.89)
+
+// let firstName = "mistura";
+// let lastName = "apostle";
+// let Number = 45;
+// const lastContent = "@gmail.com";
+// // let myGmail = `${firstName}${lastName}${lastContent}`
+
+// let newNumber = number.toString()
+
+// let myGmail = firstName + lastName +lastContent
+// console.log(myGmail);
+
+// let firstName = "apostle";
+// let lastName = "jame";
+// let name1 = parseInt(firstName)
+// let name2 = parseInt(lastName)
+
+// console.log(name1);
+// console.log(name2);
+// console.log(typeof name1);
+// console.log(typeof name2);
+
+// let number = 3.14287145367526;
+
+// let newNumber = number.toFixed(5)
+// console.log(newNumber);
+
+// let AcctBal = 10000
+// let newBal= ''
+// let amount = '';
+
+// // recharge card of 500
+// amount = '500.50'
+
+// newBal = AcctBal - parseFloat(amount);
+// console.log(newBal.tofixed(2));
+
+// let amount = document.getElementById('amount').value;
+// let TranAmount = document.getElementById(TranAmount);
+// let Balance = document.getElementById(bal);
+// let Current = document.getElementById(current);
+// let AcctBal = 10000
+// let newBal = AcctBal - parseFloat(amount)
+
+// console.log(amount)
+
+// TranAmount.innerHTML = amount
+// Balance.innerHTML = bal
+// Current.innerHTML  = AcctBal
+
+
+// let firstNum = 'my';
+// let secondNum = 'name';
+// firstNum += secondNum ;
+// console.log(firstNum);
+
+// let gender = document.getElementById("sex").value;
+// let body = document.getElementById("body");
+
+// if (gender === "male") {
+//     body.style.backgroundcolor = "blue"
+    
+// } else if (gender === "female"){
+//     body.style.backgroundcolor = "pink"
+// } else(){
+//     body.style.backgroundcolor = "pink"
+// }
+
+let names = "mat"
+let gender = 'male'
+console.log(names === "mats" ? true : false);
+
